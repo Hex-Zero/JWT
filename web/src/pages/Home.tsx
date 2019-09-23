bd259d721a905f3ba0d95bb4fbf260b6c222eda3
Hex-Zero <hex0@live.com>
2019-09-23 20:57:29 +0100

    login

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useUsersQuery } from "../generated/graphql";
 
 interface Props {}
 
 const Home: React.FC<Props> = ({}) => {
-  return <div>Home</div>;
+  const { data } = useUsersQuery({ fetchPolicy: "network-only" });
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      users:{" "}
+      <ul>
+        {data.users.map(x => {
+          return (
+            <li key={x.id}>
+              {x.email}, {x.id}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { useRegisterMutation } from "../generated/graphql";
 
-interface Props {}
+const Register: React.FC<RouteComponentProps> = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [register] = useRegisterMutation();
 
-const Register: React.FC<Props> = ({}) => {
-  return <div>Register</div>;
+  return (
+    <form
+      onSubmit={async e => {
+        e.preventDefault();
+        console.log("form Submited");
+        const response = await register({
+          variables: {
+            email,
+            password
+          }
+        });
+        console.log(response);
+
+        history.push("/");
+      }}
+    >
+      <input
+        value={email}
+        placeholder="email"
+        onChange={e => {
+          setEmail(e.target.value);
+        }}
+      />
+      <input
+        type="password"
+        value={password}
+        placeholder="password"
+        onChange={e => {
+          setPassword(e.target.value);
+        }}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
 };
 
 export default Register;
