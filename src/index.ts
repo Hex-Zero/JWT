064916eb2f1a6fd565b5388a5367a9840743e14a
Hex-Zero <hex0@live.com>
2019-09-23 17:16:35 +0100

    Refresh token generates new access token

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,39 @@
 import { ApolloServer } from "apollo-server-express";
+import cookieParser from "cookie-parser";
 import "dotenv/config";
 import express from "express";
+import { verify } from "jsonwebtoken";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnections } from "typeorm";
+import { createAccessToken } from "./auth";
+import { User } from "./entity/User";
 import { UserResolver } from "./UserResolver";
 
 (async () => {
   const app = express();
+  app.use(cookieParser());
   app.get("/", (_req, res) => res.send("hello"));
+  app.post("/refresh_token", async (req, res) => {
+    const token = req.cookies.jid;
+    if (!token) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+    let payload: any = null;
+    try {
+      payload = verify(token, process.env.REFRESH_TOKEN_SECRET!);
+    } catch (error) {
+      console.log(error);
 
+      return res.send({ ok: false, accessToken: "" });
+    }
+    const user = await User.findOne({ id: payload.userId });
+    if (!user) {
+      return res.send({ ok: false, accessToken: "" });
+    }
+
+    return res.send({ ok: true, accessToken: createAccessToken(user) });
+  });
   await createConnections();
 
   const apolloServer = new ApolloServer({
